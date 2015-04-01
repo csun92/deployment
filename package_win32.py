c58fc41254107f71ce872144afbd61d5b0699f75
unknown <sun@sun-win81-i386.ipbrech.local>
2015-04-01 17:42:02 +0200

    script module "install" complete(except 'Patch files with original install dir')

--- package_win32.py
+++ package_win32.py
@@ -16,9 +16,16 @@ def build(binary_dir, jobs):
     subprocess.check_call(["mingw32-make", "-j", str(jobs)], cwd=binary_dir)
 
 def install(binary_dir, install_dir):
-    install_dir = os.path.join(install_dir, "usr")
-
+    # No need to add path "usr"
+    # binary_dir = "F:/workspace/ITK-MinGW/ITK-build/"
+    # install_dir = "F:/workspace/ITK-MinGW/install/"
+    # Always use the above 2 paths for testing
+    
     # Patch install files that use absolute paths
+    # Attention use string format like "\\"
+    # For path, we can use (for example) "C:/" instead of "C:\"(except in shell)
+
+#Not yet needed. No particular requirement. 
     for dirpath, dirnames, filenames in os.walk(binary_dir):
         targets = [
             "install_wrapitk_compatibility.cmake",
@@ -27,10 +34,9 @@ def install(binary_dir, install_dir):
         for target in targets:
             if target in filenames:
                 filename = os.path.join(dirpath, target)
-                print filename
                 
                 pattern = re.compile(r'file\(INSTALL DESTINATION \"C:/', re.I)
-                repl = "file(INSTALL DESTINATION \"{}C:/".format(install_dir)
+                repl = "file(INSTALL DESTINATION \"{}".format(install_dir)
                 
                 with open(filename,"r+") as fs:
 					before = fs.read()
@@ -38,5 +44,32 @@ def install(binary_dir, install_dir):
 					fs.seek(0)
 					fs.truncate(0)
 					fs.write(after)
-					fs.close()
 
+    # Install
+    subprocess.check_call([
+        "cmake", 
+        "-DCMAKE_INSTALL_PREFIX:PATH={}".format(install_dir),
+        "-P", os.path.join(binary_dir, "cmake_install.cmake")
+    ])
+
+    # No need to Fix install in PREFIX/usr
+
+    # No need to Patch files that force downloading external projects,
+    # since these projects were built included
+    
+    # Patch files with original install dir
+    subprocess.check_call([
+        "sed", "-i", 
+        """s@\(set(possible_itk_dir\) ".*")@\\1 "/usr/lib/cmake/ITK-4.6")@i""",
+        os.path.join(install_dir, "lib/cmake/ITK-4.6/WrapITK/WrapITKConfig.cmake")
+    ])
+
+    # Not Found: No need to Fix PYTHONPATH for pygccxml
+
+    # Add missing files
+    # * Wrapping/Typedefs/*SwigInterface.h -> usr/lib/cmake/ITK-4.6/WrapITK/Configuration/Typedefs/
+    typedefs_source = os.path.join(binary_dir, "Wrapping", "Typedefs")
+    typedefs_dest = os.path.join(install_dir, 
+        "lib", "cmake", "ITK-4.6", "WrapITK", "Configuration", "Typedefs")
+    for filename in glob.glob(os.path.join(typedefs_source, "*SwigInterface.h")):
+        shutil.copy(filename, typedefs_dest)
