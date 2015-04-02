b301952f5a93c2d8ccbf01a7d577b405883b4c23
unknown <sun@sun-win81-i386.ipbrech.local>
2015-04-02 17:53:59 +0200

    Add module package() and unpack()

--- package_win32.py
+++ package_win32.py
@@ -3,6 +3,8 @@ import glob
 import os
 import shutil
 import subprocess
+import re
+import zipfile
 
 
 def Directory(value):
@@ -10,22 +12,19 @@ def Directory(value):
         raise argparse.ArgumentTypeError("No such directory: '{}'".format(value))
     return os.path.abspath(value)
 
-
-
 def build(binary_dir, jobs):
     subprocess.check_call(["mingw32-make", "-j", str(jobs)], cwd=binary_dir)
 
 def install(binary_dir, install_dir):
     # No need to add path "usr"
     # binary_dir = "F:/workspace/ITK-MinGW/ITK-build/"
-    # install_dir = "F:/workspace/ITK-MinGW/install/"
+    # install_dir = "C:/ITK-4.6/"
     # Always use the above 2 paths for testing
     
     # Patch install files that use absolute paths
     # Attention use string format like "\\"
     # For path, we can use (for example) "C:/" instead of "C:\"(except in shell)
 
-#Not yet needed. No particular requirement. 
     for dirpath, dirnames, filenames in os.walk(binary_dir):
         targets = [
             "install_wrapitk_compatibility.cmake",
@@ -58,13 +57,21 @@ def install(binary_dir, install_dir):
     # since these projects were built included
     
     # Patch files with original install dir
-    subprocess.check_call([
-        "sed", "-i", 
-        """s@\(set(possible_itk_dir\) ".*")@\\1 "/usr/lib/cmake/ITK-4.6")@i""",
-        os.path.join(install_dir, "lib/cmake/ITK-4.6/WrapITK/WrapITKConfig.cmake")
-    ])
+    patchfile = os.path.join(install_dir, "lib/cmake/ITK-4.6/WrapITK/WrapITKConfig.cmake")
+    pattern = re.compile(r'set\(possible_itk_dir.*')
+    new_path = os.path.join(install_dir, "lib/cmake/ITK-4.6")
+    repl = "set(possible_itk_dir \"{}\")".format(new_path)
+    with open(patchfile, "r+") as fs:
+        before = fs.read()
+        after = re.sub(pattern,repl,before)
+        fs.seek(0)
+        fs.truncate(0)
+        fs.write(after)
 
-    # Not Found: No need to Fix PYTHONPATH for pygccxml
+    # Fix PYTHONPATH for pygccxml
+    with open(os.path.join(install_dir, "tools/python2/Lib/site-packages/WrapITK.pth"), "a") as fd:
+        p = os.path.join(install_dir,"lib/cmake/ITK-4.6/WrapITK/Configuration/Generators/SwigInterface/src")
+        fd.write(p+"\n")
 
     # Add missing files
     # * Wrapping/Typedefs/*SwigInterface.h -> usr/lib/cmake/ITK-4.6/WrapITK/Configuration/Typedefs/
@@ -73,3 +80,38 @@ def install(binary_dir, install_dir):
         "lib", "cmake", "ITK-4.6", "WrapITK", "Configuration", "Typedefs")
     for filename in glob.glob(os.path.join(typedefs_source, "*SwigInterface.h")):
         shutil.copy(filename, typedefs_dest)
+
+def package(install_dir):
+    package_name = "ITK-4.6.zip"
+    with zipfile.ZipFile(package_name, 'w', zipfile.ZIP_DEFLATED) as itkpack:
+        for dirpath,dirnames,filenames in os.walk(install_dir):
+            for filename in filenames:
+                fullpath = os.path.join(dirpath,filename)
+                itkpack.write(fullpath)
+                
+    
+def unpack():
+    package_name = "ITK-4.6.zip"
+    # Get python path
+    pypath = subprocess.check_output(["where", "python"]).strip()
+    dest = "C:/" #Always absolute path
+    # Attention cmake path is considered by default "C:/Program Files/CMake/"
+    pattern_cmake = re.compile(r'ITK-4.6/Program Files/CMake/', re.I)
+    pattern_itk = re.compile(r'ITK-4.6/Program Files/ITK', re.I)
+    pattern_py = re.compile(r'ITK-4.6/tools/', re.I)
+    with zipfile.ZipFile(package_name, 'r') as itkpack:
+        filelist = itkpack.namelist()
+        for member in filelist:
+            if re.search(pattern_py, member):
+                pass
+            elif re.search(pattern_itk, member):
+                itk_dest = os.path.join(dest, member.split('/',1)[1])
+                itkpack.extract(member, itk_dest)
+            else:
+                itkpack.extract(member, dest)
+        
+
+
+
+
+    
