2960111799157912ce5b8d637625af6f5c46c041
unknown <sun@sun-win81-i386.ipbrech.local>
2015-04-09 15:14:44 +0200

    Complete package_win32.py test success with use_system_dcmtk:off

--- package_win32.py
+++ package_win32.py
@@ -5,6 +5,8 @@ import shutil
 import subprocess
 import re
 import zipfile
+import hashlib
+import urllib2
 
 
 def Directory(value):
@@ -12,6 +14,75 @@ def Directory(value):
         raise argparse.ArgumentTypeError("No such directory: '{}'".format(value))
     return os.path.abspath(value)
 
+def setup_parser(parser):
+    parser.add_argument("root", help="Root directory", type=Directory)
+    parser.add_argument("-j", "--jobs", type=int, default=1,
+        help="Number of parallel make jobs")
+    parser.add_argument("maintainer", 
+        help="Package maintainer (e.g. John Doe <doe@example.com>)")
+    
+def run(upstream_version, local_revision, root, maintainer, jobs):
+    
+    try:
+        step = "download"
+        source_dir = download(upstream_version, root)
+        
+        step = "configure"
+        binary_dir = os.path.join(root, "build")
+        if not os.path.isfile(os.path.join(binary_dir, "CMakeCache.txt")):
+            configure(source_dir, binary_dir)
+        
+        step = "build"
+        build(binary_dir, jobs)
+        
+        step = "install"
+        install_dir = os.path.join(root, "install")
+        install(binary_dir, install_dir)
+        
+        step = "package"
+        package(install_dir)
+    
+    except Exception, e:
+        print "Could not {}: {}".format(step, e)
+    
+
+def download(version, directory):
+
+    sha1 = {
+        "4.6.1": "3074e324f786339947dc002d0ec883a357e48825",
+    }
+
+    if version not in sha1:
+        raise Exception("Unknown version: {}".format(version))
+
+    archive = "InsightToolkit-{}.zip".format(version)
+    if not os.path.isfile(os.path.join(directory, archive)):
+        url = "http://sourceforge.net/projects/itk/files/itk/{}/{}/download".format(
+            version[:3], archive)
+        source = urllib2.urlopen(url)
+        output = open(os.path.join(directory, archive),'wb')
+        output.write(source.read())
+        output.close()
+
+        #check signature
+        BLOCKSIZE = 65536
+        hasher = hashlib.sha1()
+        with open(archive, 'rb') as pkg:
+            buf = pkg.read(BLOCKSIZE)
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = pkg.read(BLOCKSIZE)
+        print hasher.hexdigest()
+        
+    source_dir = os.path.join(directory, "InsightToolkit-{}".format(version))
+    if not os.path.isdir(source_dir):
+        with zipfile.ZipFile(archive, 'r') as pkg:
+            pkg.extractall(directory)
+
+    return source_dir
+
+#Attention, DCMTK must be installed before configure.
+#If not, set ITK_USE_SYSTEM_DCMTK: OFF(might cause compile errors)
 def configure(source_dir, build_dir):
     options = {}
     
@@ -24,7 +95,7 @@ def configure(source_dir, build_dir):
         "CMAKE_SKIP_RPATH": "ON"
     })
     
-    # Use system tools as much as possible. HDF5 fails on Ubuntu 12.04
+    # Use system tools as much as possible.
     options.update({
         "ITK_USE_SYSTEM_DCMTK": "ON"
     })
@@ -38,7 +109,7 @@ def configure(source_dir, build_dir):
     }
     for module, value in modules.items():
         options["Module_ITK{}".format(module)] = "ON" if value else "OFF"
-
+    
     # Wrapping
     options.update({
         "ITK_WRAPPING": "ON",
@@ -56,7 +127,7 @@ def configure(source_dir, build_dir):
         "rgba_unsigned_char": True, "rgba_unsigned_short": False,
         # Integer types
         "signed_char": True, "signed_long": True, "signed_short": True,
-        "unsigned_char": True, "unsigned_long": True, "unsigned_short": True,
+        "unsigned_char": True, "unsigned_long": True, "unsigned_short": True
     }
     for type_, value in types.items():
         options["ITK_WRAP_{}".format(type_)] = "ON" if value else "OFF"     
@@ -70,10 +141,12 @@ def configure(source_dir, build_dir):
     
     options = ["-D{}={}".format(name, value) 
         for name, value in options.items()]
+    #specify MinGW as generator(instead of VS)
+    generator = "-GMinGW Makefiles"
     
     if not os.path.isdir(build_dir):
         os.makedirs(build_dir)
-    subprocess.check_call(["cmake"] + options + [source_dir],
+    subprocess.check_call(["cmake"] + options + [generator] + [source_dir],
         cwd=build_dir)
 
 def build(binary_dir, jobs):
